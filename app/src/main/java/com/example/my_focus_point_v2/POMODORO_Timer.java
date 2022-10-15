e24a5c3d4ceb9fc2677b1f0d087517bf7ff366ee
Mangalam Sharma <mangalamsharma2050@gmail.com>
2022-10-15 23:16:32 +0530

    first commit

---
+++ app/src/main/java/com/example/my_focus_point_v2/POMODORO_Timer.java
@@ -0,0 +1,14 @@
+package com.example.my_focus_point_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class POMODORO_Timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pomodoro_timer);
+    }
+}
---
+++ app/src/main/java/com/example/my_focus_point_v2/SIGNUP_Mainframe.java
@@ -0,0 +1,14 @@
+package com.example.my_focus_point_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SIGNUP_Mainframe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_mainframe);
+    }
+}
